8b0b8931f06857148956e52eb095984bd9769023
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-06 12:52:04 -1100

    Display mobile menu

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const menuBtn = document.querySelector('.hamburger_icon');
 const navBar = document.querySelector('nav');
+const menuItems = document.querySelector('nav li a');
+
+menuBtn.addEventListener('click', displayMenu)
+
+function displayMenu() {
+  navBar.classList.toggle('show_menu');
+}
+
+
+
