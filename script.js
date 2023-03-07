a35929bf1166b15bda996d640bb21ea3a6597f6a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-06 14:51:20 -1100

    Fix eslint issues

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const menuBtn = document.querySelector('.hamburger_icon');
 const navBar = document.querySelector('nav');
 const menuItems = [...document.querySelectorAll('nav li a')];
 
-menuBtn.addEventListener('click', displayMenu)
-menuItems.forEach(item => item.addEventListener('click', displayMenuSection))
+function toggleMenuBtn() {
+  if (menuBtn.src.includes('hamburger')) {
+    menuBtn.src = 'img/cancel-icon.svg';
+  } else {
+    menuBtn.src = 'img/hamburger-icon.svg';
+  }
+}
 
 function displayMenu() {
-  if (window.innerWidth < 768)
-  navBar.classList.toggle('show_menu');
-  toggleMenuBtn()
+  if (window.innerWidth < 768) { navBar.classList.toggle('show_menu'); }
+  toggleMenuBtn();
 }
 
 function displayMenuSection() {
@@ -16,12 +20,5 @@ function displayMenuSection() {
   menuBtn.src = 'img/hamburger-icon.svg';
 }
 
-function toggleMenuBtn() {
-  if (menuBtn.src.includes('hamburger')) {
-    menuBtn.src = 'img/cancel-icon.svg'
-  } else {
-    menuBtn.src = 'img/hamburger-icon.svg'
-  }
-}
-
-
+menuBtn.addEventListener('click', displayMenu);
+menuItems.forEach((item) => item.addEventListener('click', displayMenuSection));
