06fe9f5517d4c8db77b150423d83877cde2af2fb
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-06 13:01:46 -1100

    Change menu button when clicked

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const menuBtn = document.querySelector('.hamburger_icon');
 const navBar = document.querySelector('nav');
-const menuItems = document.querySelector('nav li a');
+const menuItems = [...document.querySelectorAll('nav li a')];
 
 menuBtn.addEventListener('click', displayMenu)
+menuItems.forEach(item => item.addEventListener('click', displayMenu))
 
 function displayMenu() {
   navBar.classList.toggle('show_menu');
+  toggleMenuBtn()
 }
 
+function toggleMenuBtn() {
+  if (menuBtn.src.includes('hamburger')) {
+    menuBtn.src = 'img/cancel-icon.svg'
+  } else {
+    menuBtn.src = 'img/hamburger-icon.svg'
+  }
+  menuBtn.style.zIndex = 2
+}
 
 
